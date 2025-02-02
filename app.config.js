306f27443cf5f168c03ece10b06ceec67858ba3a
Hamdi-Mohamed-BE <mohamed@pixelfield.cz>
2025-02-02 19:07:42 +0100

    building

--- app.config.js
+++ app.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   expo: {
     // ... other config
+    name: 'valorant-tips',
+    slug: 'valorant-tips',
+    version: '1.0.0',
+    owner: 'hama.gn',
+    cli: {
+      appVersionSource: "native-version"
+    },
     extra: {
       YOUTUBE_API_KEY: process.env.EXPO_PUBLIC_YOUTUBE_API_KEY,
+      eas: {
+        projectId: 'd7220946-46e8-4546-8863-a71ab0dc7ae9'
+      },
+    },
+    android: {
+      package: 'com.hamagn.valoranttpis',
+      versionCode: 1,
     },
   },
 }; 
